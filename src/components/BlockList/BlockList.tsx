465f6332cdec875311e1bdedc71f272eeefb4e54
ZeroYangxi <riveryangxi@gmail.com>
2024-03-29 15:05:15 -0400

    apply grid; create Dates component

--- src/components/BlockList/BlockList.tsx
+++ src/components/BlockList/BlockList.tsx
@@ -3,14 +3,22 @@ import styles from "./BlockList.module.css";
 
 const BlockList = () => {
   return (
-    <div className={styles.blockList}>
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.header}>Header1</div>
+        <div className={styles.header}>date</div>
+        <div className={styles.header}>Header3</div>
+        <div className={styles.taskName}>Task Name</div>
+        <div className={styles.blockList}>
+          <Block />
+          <Block />
+          <Block />
+          <Block />
+          <Block />
+          <Block />
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Dates/Dates.tsx
@@ -0,0 +1,7 @@
+import styles from "./Dates.module.css";
+
+const Dates = () => {
+  return <div className={styles.dates}>Dates</div>;
+};
+
+export default Dates;
